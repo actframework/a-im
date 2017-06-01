df53a29ed5a1d4143443ec1a2ee2c581d06cbbd0
Gelin Luo <greenlaw110@gmail.com>
2017-06-01 12:30:00 +1000

    wiip

--- svc/src/main/java/chatroom/security/AppSecurity.java
+++ svc/src/main/java/chatroom/security/AppSecurity.java
@@ -1,37 +1,19 @@
 package chatroom.security;
 
 import act.aaa.ActAAAService;
+import act.util.Stateless;
 import chatroom.model.User;
-import org.osgl.aaa.Principal;
-import org.osgl.util.C;
-
-import javax.inject.Singleton;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 
 /**
  * Plugin the aaa security framework
  */
-@Singleton
-public class AppSecurity extends ActAAAService.Base<User> implements AppPrivileges {
-
-    private ConcurrentMap<String, User> userCache = new ConcurrentHashMap<>();
-
-    public AppSecurity() {
-        System.out.println("");
-    }
+@Stateless
+public final class AppSecurity extends ActAAAService.Base<User> implements AppPrivileges {
 
     @Override
-    protected final String username(User user) {
+    protected String username(User user) {
         return user.email;
-    }
-
-    @Override
-    protected final Set<String> permissionsOf(User user) {
-        return C.set();
     }
 
     @Override
@@ -40,31 +22,8 @@ public class AppSecurity extends ActAAAService.Base<User> implements AppPrivileg
     }
 
     @Override
-    protected Set<String> rolesOf(User user) {
-        return super.rolesOf(user);
-    }
-
-    @Override
-    protected final boolean verifyPassword(User user, char[] chars) {
+    protected boolean verifyPassword(User user, char[] chars) {
         return user.verifyPassword(new String(chars));
-    }
-
-    @Override
-    protected void setPrincipalProperties(Principal principal, User user) {
-        userCache.put(principal.getName(), user);
-    }
-
-    public User getUserFromPrincipal(Principal principal) {
-        return userCache.get(principal.getName());
-    }
-
-    /**
-     * Returns permissions granted to every logged in user
-     * @return the set of common permissions
-     */
-    public static Set<String> newCommonPermissions() {
-        Set<String> perms = new HashSet<>();
-        return perms;
     }
 
 }
